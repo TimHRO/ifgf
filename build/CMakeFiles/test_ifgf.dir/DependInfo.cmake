213a94dad95b97fad48532defc96dd72488adbf3
TimHRO <e12435600@student.tuwien.ac.at>
2026-05-15 08:20:57 +0200

    Prepare loop over cones

---
+++ build/CMakeFiles/test_ifgf.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/tim/Desktop/ifgf old stuff/ifgf/test_ifgf.cpp" "CMakeFiles/test_ifgf.dir/test_ifgf.cpp.o" "gcc" "CMakeFiles/test_ifgf.dir/test_ifgf.cpp.o.d"
+  "" "test_ifgf" "gcc" "CMakeFiles/test_ifgf.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/test_ifgf.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for test_ifgf.
+# This may be replaced when dependencies are built.
